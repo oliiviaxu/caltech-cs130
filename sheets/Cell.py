585b39621d2bcb3e019e4956c73fd41a03007807
oliiviaxu <olivia.xu@gmail.com>
2025-01-14 21:25:53 -0500

    oops

--- sheets/Cell.py
+++ sheets/Cell.py
@@ -1,7 +1,7 @@
 class Cell:
     def __init__(self, contents=''):
         self.cell_type = None
-        self.contents = ''
+        self.contents = contents
         self.value = ''
         self.outgoing = []
         self.ingoing = []
