29eaeab8a7a1ab3047bc7d3c29f6a296d5ac212f
wilson-duan <wilson.duan@memverge.com>
2025-01-14 18:26:35 -0800

    tests for cell contents

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,4 @@
+# python -m unittest discover -s tests
 import unittest
 import sheets
 
@@ -18,7 +19,24 @@ class BasicTests(unittest.TestCase):
             wb.new_sheet(' Sheet')
         with self.assertRaises(ValueError):
             wb.new_sheet('~')
+    
+    def test_set_cell_contents(self):
+        wb = sheets.Workbook()
+        wb.new_sheet()
+        wb.set_cell_contents('Sheet1', 'D5', 'test')
+        self.assertEqual(wb.get_sheet_extent('Sheet1'), (4, 5))
+        wb.set_cell_contents('Sheet1', 'C6', 'test')
+        self.assertEqual(wb.get_sheet_extent('Sheet1'), (4, 6)) # test adding rows
+        wb.set_cell_contents('Sheet1', 'E4', '  test  ')
+        self.assertEqual(wb.get_sheet_extent('Sheet1'), (5, 6)) # test adding columns
+
+        with self.assertRaises(KeyError):
+            wb.set_cell_contents('Sheet2', 'D5', 'test')
+        with self.assertRaises(ValueError):
+            wb.set_cell_contents('Sheet1', 'D5D5', 'test')
         
+        self.assertEqual(wb.get_cell_contents('Sheet1', 'D5'), 'test') # test basic get
+        self.assertEqual(wb.get_cell_contents('Sheet1', 'E4'), 'test') # test that trimming whitespace worked
 
 if __name__ == "__main__":
     unittest.main()
