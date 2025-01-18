42cf8418eaa3b1ccb25f1bd34fbd8a554e403301
oliiviaxu <olivia.xu@gmail.com>
2025-01-18 00:16:45 -0500

    starter interpreter

--- sheets/interpreter.py
+++ sheets/interpreter.py
@@ -1,10 +1,19 @@
 import decimal
 import lark
-# from lark.visitors import visit_children_decor
+from lark.visitors import visit_children_decor
 
 class FormulaEvaluator(lark.visitors.Interpreter):
+
+    error_dict = {
+        "#ERROR!": 1,
+        "#CIRCREF!": 2,
+        "#REF!": 3,
+        "#NAME?": 4,
+        "#VALUE!": 5,
+        "#DIV/0!": 6
+    }
     
-    # @visit_children_decor
+    @visit_children_decor
     def add_expr(self, values):
         if values[1] == '+':
             return values[0] + values[2]
@@ -12,6 +21,35 @@ class FormulaEvaluator(lark.visitors.Interpreter):
             return values[0] - values[2]
         else:
             assert False, f'Unexpected operation: {values[1]}'
+    
+    @visit_children_decor
+    def mul_expr(self, values):
+        if values[1] == '*':
+            return values[0] * values[2]
+        elif values[1] == '/':
+            return values[0] / values[2]
+        else:
+            assert False, f'Unexpected operation: {values[1]}'
+    
+    @visit_children_decor
+    def unary_op(self, values):
+        if values[0] == '+':
+            return abs(values[1])
+        elif values[0] == '-':
+            return -abs(values[1])
+        else:
+            assert False, f'Unexpected operation: {values[0]}'
+
+    @visit_children_decor
+    def concat_expr(self, values):
+        if values[1] == '&':
+            return values[0] + values[2]
+        else:
+            False, f'Unexpected operation: {values[1]}'
+
+    def error(self, tree):
+        error_value = FormulaEvaluator.error_dict[self.children[0]]
+        return error_value
 
     def parens(self, tree):
         values = self.visit_children(tree)
@@ -19,9 +57,8 @@ class FormulaEvaluator(lark.visitors.Interpreter):
     
     def number(self, tree):
         # called when run into number node 
-        return decimal.Decimal(tree.children)
+        return decimal.Decimal(tree.children[0])
 
     def string(self, tree):
         # called when run into a string node
         return tree.children[0].value[1:-1]
-    
