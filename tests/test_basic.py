c50459a83bab093a6b5bfae8d14baf906066f2af
wilson-duan <wilson.duan@memverge.com>
2025-02-02 12:48:37 -0800

    improve tests for save/load wb

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -642,27 +642,31 @@ class BasicTests(unittest.TestCase):
         file_path = os.path.join(json_dir, 'simple.json')
         with open(file_path, "r") as file:
             wb = sheets.Workbook.load_workbook(file)
-            self.assertEqual(wb.get_sheet_extent('Sheet1'), (3, 1))
+            self.assertEqual(wb.get_sheet_extent('Sheet1'), (4, 1))
             self.assertEqual(wb.get_cell_contents('Sheet1', 'A1'), '\'123')
             self.assertEqual(wb.get_cell_contents('Sheet1', 'B1'), '5.3')
             self.assertEqual(wb.get_cell_contents('Sheet1', 'C1'), '=A1*B1')
             self.assertEqual(wb.get_cell_value('Sheet1', 'C1'), decimal.Decimal('651.9'))
+            self.assertEqual(wb.get_cell_contents('Sheet1', 'D1'), '=\"double\" & \" quote\"')
+            self.assertEqual(wb.get_cell_value('Sheet1', 'D1'), 'double quote')
 
             self.assertEqual(wb.get_sheet_extent('Sheet2'), (3, 1))
             self.assertEqual(wb.get_cell_contents('Sheet2', 'A1'), '4.3')
             self.assertEqual(wb.get_cell_contents('Sheet2', 'B1'), '\'testing')
             self.assertEqual(wb.get_cell_contents('Sheet2', 'C1'), '=A1&B1')
             self.assertEqual(wb.get_cell_value('Sheet2', 'C1'), '4.3testing')
-
-        # test same but with txt
+        
+        # check with txt
         file_path = os.path.join(json_dir, 'simple.txt')
         with open(file_path, "r") as file:
             wb = sheets.Workbook.load_workbook(file)
-            self.assertEqual(wb.get_sheet_extent('Sheet1'), (3, 1))
+            self.assertEqual(wb.get_sheet_extent('Sheet1'), (4, 1))
             self.assertEqual(wb.get_cell_contents('Sheet1', 'A1'), '\'123')
             self.assertEqual(wb.get_cell_contents('Sheet1', 'B1'), '5.3')
             self.assertEqual(wb.get_cell_contents('Sheet1', 'C1'), '=A1*B1')
             self.assertEqual(wb.get_cell_value('Sheet1', 'C1'), decimal.Decimal('651.9'))
+            self.assertEqual(wb.get_cell_contents('Sheet1', 'D1'), '=\"double\" & \" quote\"')
+            self.assertEqual(wb.get_cell_value('Sheet1', 'D1'), 'double quote')
 
             self.assertEqual(wb.get_sheet_extent('Sheet2'), (3, 1))
             self.assertEqual(wb.get_cell_contents('Sheet2', 'A1'), '4.3')
@@ -693,6 +697,7 @@ class BasicTests(unittest.TestCase):
         wb.set_cell_contents('Sheet1', 'A1', '=B1 + C1')
         wb.set_cell_contents('Sheet1', 'B1', '=\'3')
         wb.set_cell_contents('MySheet', 'A1', '=Sheet1!A1 + 2')
+        wb.set_cell_contents('MySheet', 'B1', '="testing" & " double"')
 
         file_path = os.path.join(json_dir, 'writing.json')
 
@@ -708,6 +713,7 @@ class BasicTests(unittest.TestCase):
 
             self.assertEqual(sheet_dic[1]["name"], "MySheet")
             self.assertEqual(sheet_dic[1]["cell-contents"]["A1"], "=Sheet1!A1 + 2")
+            self.assertEqual(sheet_dic[1]["cell-contents"]["B1"], "=\"testing\" & \" double\"")
         
     def test_move_sheet(self):
         pass
@@ -724,3 +730,4 @@ if __name__ == "__main__":
 # TODO: change ingoing and outgoing to sets (not arrays)
     # maybe write a test that checks if setting =A2 + A2 works correctly
 # TODO: Test a variety of formulas that include multiple operators between both cells and numbers
+# Always output a "sheets" list, even for empty workbooks. The list will simply contain no sheets for an empty workbook. Similarly, a sheet with no cells set should still have an empty "cell-contents" dictionary in the JSON output.
