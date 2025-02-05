90a2932a30a109ca5c9cdd75dc180367fdec6ada
oliiviaxu <olivia.xu@gmail.com>
2025-02-04 22:20:40 -0800

    updated init.py in test suite

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,3 +3,5 @@ from .test_errors import ErrorTests
 from .test_json import JsonTests
 from .test_smoke import SmokeTest
 from .test_spreadsheet import SpreadsheetTests
+from .test_cycles import CycleDetectionTests
+# from .test_updates import CellUpdateTests
