33e4d26358dc32956e865a926c245de870ced44f
oliiviaxu <olivia.xu@gmail.com>
2025-02-05 10:45:49 -0800

    more cell update propagation tests

--- tests/test_updates.py
+++ tests/test_updates.py
@@ -23,15 +23,20 @@ class CellUpdateTests(unittest.TestCase):
                 wb.set_cell_contents(sheet_name, f'A{i}', f'=A{i+1} + {1}')
             # Set the last cell to a number
             wb.set_cell_contents(sheet_name, f'A{num_cells}', str(cell_val))
-        
         elif structure_type == StructureType.WEB.value:
             # many cells depend on this cell
             src_cell, src_val = 'A1', '12'
             wb.set_cell_contents(sheet_name, src_cell, src_val)
 
+            sn, counter = sheet_name, 1
             for i in range(1, num_cells):
-                wb.set_cell_contents(sheet_name, f'{chr(65 + (i % 26))}1', f'={src_cell} + 1')
-
+                if i % 26 == 0:
+                    counter += 1
+                    wb.new_sheet(f'Sheet{counter}')
+                    sn = f'Sheet{counter}'
+                curr_cell = f'{chr(65 + (i % 26))}1'
+                wb.set_cell_contents(sn, curr_cell, f'={sheet_name}!{src_cell} + 1')
+            
     def test_long_chain(self):
         wb = sheets.Workbook()
         wb.new_sheet()
@@ -43,18 +48,34 @@ class CellUpdateTests(unittest.TestCase):
         for i in range(1, num_cells + 1):
             expected_value = 20 + (num_cells - i)
             self.assertEqual(wb.get_cell_value('sheet1', f'A{i}'), decimal.Decimal(expected_value))
+        
+    def many_refs_helper(self, wb, sheet_name, num_cells, src_val):
+        sn = sheet_name
+        counter = 1
+        for i in range(1, num_cells):
+            if i % 26 == 0:
+                counter += 1
+                sn = f'Sheet{counter}'
+            curr_cell = f'{chr(65 + (i % 26))}1'
+            self.assertEqual(wb.get_cell_value(sn, curr_cell), decimal.Decimal(int(src_val) + 1))
 
     def test_many_refs(self):
         wb = sheets.Workbook()
-        wb.new_sheet()
         num_cells = 50
-        CellUpdateTests.create_dependency_structure(wb, 'Sheet1', 2, num_cells)
+        sheet_name = 'Sheet1'
+        src_cell = 'A1' # must match line 28 TODO: fix this
+        wb.new_sheet(sheet_name)
+        CellUpdateTests.create_dependency_structure(wb, sheet_name, 2, num_cells)
 
-        wb.set_cell_contents('Sheet1', 'A1', '=1')
-        for i in range(1, num_cells):
-            cell_val = wb.get_cell_value('Sheet1', f'{chr(65 + (i % 26))}1')
-            self.assertEqual(decimal.Decimal('2'), cell_val)
-            
+        sn = sheet_name
+        self.many_refs_helper(wb, sn, num_cells, '12')
+
+        wb.set_cell_contents(sheet_name, src_cell, '1')
+        self.many_refs_helper(wb, sn, num_cells, '1')
+
+        wb.set_cell_contents(sheet_name, src_cell, '=5000*5000')
+        self.many_refs_helper(wb, sn, num_cells, f'{5000*5000}')
+
         # TODO:Alternately, you might also have updates where each cell is referenced by many other cells, 
         # perhaps with much shallower chains, but still with large amounts of cell updates. 
 
