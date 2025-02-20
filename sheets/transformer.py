1fc180280d206db48beb3a445b7729c7c48e5ed4
oliiviaxu <olivia.xu@gmail.com>
2025-02-19 21:01:43 -0800

    changed formula updating transformer

--- sheets/transformer.py
+++ sheets/transformer.py
@@ -73,11 +73,6 @@ class FormulaUpdater(lark.visitors.Transformer):
     def __init__(self, delta_x, delta_y):
         self.delta_x = delta_x
         self.delta_y = delta_y
-
-    # @staticmethod
-    # def sheet_name_needs_quotes(sheet_name):
-    #     pattern = r"^[A-Za-z_][A-Za-z0-9_]*$"
-    #     return not bool(re.fullmatch(pattern, sheet_name))
     
     def mul_expr(self, tree):
         return str(tree[0]) + ' ' + str(tree[1]) + ' ' + str(tree[2]) 
@@ -140,28 +135,6 @@ class FormulaUpdater(lark.visitors.Transformer):
             return self.update_contents(str(tree[0]))
         if len(tree) == 2: # cell ref with sheetname 
             curr_name = str(tree[0])
-            # # check if has quotes.
-            # # if it has quotes and doesn't need, then remove
-            # # if it has quotes and needs, then keep
-            # if (len(curr_name) > 2 and curr_name[0] == '\'' and curr_name[-1] == '\''):
-            #     if (not SheetNameExtractor.sheet_name_needs_quotes(curr_name[1:-1])):
-            #         curr_name = curr_name[1:-1]
-            #         if curr_name == self.sheet_name:
-            #             curr_name = self.new_sheet_name
-            #             if SheetNameExtractor.sheet_name_needs_quotes(curr_name):
-            #                 curr_name = "'" + curr_name + "'"
-            #     else:
-            #         # it needs quotes
-            #         if curr_name[1:-1] == self.sheet_name:
-            #             curr_name = self.new_sheet_name
-            #             if SheetNameExtractor.sheet_name_needs_quotes(curr_name):
-            #                 curr_name = "'" + curr_name + "'"
-            # else:
-            #     if curr_name == self.sheet_name:
-            #         curr_name = self.new_sheet_name
-            #         if SheetNameExtractor.sheet_name_needs_quotes(curr_name):
-            #             curr_name = "'" + curr_name + "'"
-
             formula = self.update_contents(str(tree[1]))
             return curr_name + '!' + formula
         else:
